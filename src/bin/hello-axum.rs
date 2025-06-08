41840636f20211ca5506311ddfda1c2aa3fd5a9a
Marcin Kolenda <marcinkolenda419@gmail.com>
2025-06-08 20:39:04 +0200

    Add axum dependency and `hello-axum` target
    This commit introduces a basic axum web server example to the project,
    demonstrating how to set up routes, handle requests, serialize/deserialize
    data using serde, and use tracing for logging.

---
+++ src/bin/hello-axum.rs
@@ -0,0 +1,53 @@
+use axum::{
+    http::StatusCode, response::Redirect, routing::{get, post}, Json, Router
+};
+use serde::{Deserialize, Serialize};
+use tracing::info;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+    .with_max_level(tracing::Level::INFO)
+    .init();
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user))
+        .fallback(anything_else);
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3123").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    info!("GET root");
+    "Hello, World!"
+}
+
+async fn create_user(
+    Json(payload): Json<CreateUser>,
+) -> (StatusCode, Json<User>) {
+    info!("POST create_user");
+
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    (StatusCode::CREATED, Json(user))
+}
+
+async fn anything_else() -> Redirect {
+    Redirect::to("/")
+}
+
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
+}
