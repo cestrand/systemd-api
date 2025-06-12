604adccd56d49ebb1866853c71cc078f5192691e
Marcin Kolenda <marcinkolenda419@gmail.com>
2025-06-12 17:57:18 +0200

    libsystemd bindings

--- build.rs
+++ build.rs
@@ -1,2 +1,17 @@
 fn main() {
+    // Generate Rust bindings for libsystemd using bindgen. The `wrapper.h`
+    // file only includes the desired systemd headers, which keeps the
+    // resulting bindings focused on the APIs we need.
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .clang_arg("-I/usr/include")
+        .generate()
+        .expect("Unable to generate bindings for systemd");
+
+    bindings
+        .write_to_file("src/bindings.rs")
+        .expect("Couldn't write bindings!");
+
+    println!("cargo:rustc-link-lib=dylib=systemd");
+    println!("cargo:rustc-link-search=native=/usr/lib64");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,13 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(unsafe_op_in_unsafe_fn)]
+pub mod version;
+
+include!("bindings.rs");
+
 use std::ffi::c_char;
 
 pub extern "C" fn systemd_version() -> *const c_char {
-    return b"257.5".as_ptr() as *const c_char
+    return b"257.5\0".as_ptr() as *const c_char
 }
